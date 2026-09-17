1f83f7ed0c564e34af7ed413900d15ebf06a6388
Juan Vargas <vargascampuzanojuanjose@gmail.com>
2026-09-17 15:46:03 -0500

    feat: setup inicial del proyecto con Vite y React

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <div>
+      <h1>
+        Consulta Facil
+      </h1>
+
+      <p>
+        Bienvenido
+      </p>
+
+      <button>
+        Buscar consultorio
+      </button>
+    </div>
+
+  )
+}
+
+export default App
